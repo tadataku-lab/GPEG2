b0d5b893dd7611a1b3b3d3227028c8e4cabe01b7
tadataku-lab <tadataku.lab@gmail.com>
2018-03-22 16:46:00 +0900

    ref: choice refactoring

--- src/gpeg_parser.rs
+++ src/gpeg_parser.rs
@@ -56,14 +56,13 @@ pub mod gpeg_parser{
 
     pub fn choice(left: Box<Fn(& ParserContext) -> bool>, right: Box<Fn(& ParserContext) -> bool>, e: Box<Fn(& ParserContext) -> bool>) -> Box<Fn(& ParserContext) -> bool> {
         Box::new(move |p: & ParserContext| -> bool {
-            let back_pos = p.state.borrow_mut().pos;
-            let mut back_tree = p.state.borrow_mut().tree.clone();
+            //let back_pos = p.state.borrow_mut().pos;
+            //let mut back_tree = p.state.borrow_mut().tree.clone();
+            let mut back_state = p.state.clone();
             if left(p) { e(p) } else{ 
-                p.state.borrow_mut().pos = back_pos;
                 {
                     let mut prev_state = p.state.borrow_mut();
-                    prev_state.tree.clear();
-                    prev_state.tree.append(&mut back_tree);
+                    prev_state.set(back_state.into_inner());
                 }
                 right(p) && e(p)
             } 
--- src/state.rs
+++ src/state.rs
@@ -2,8 +2,15 @@ pub mod state{
 
     use tree::tree::Tree;
 
+    #[derive(Debug, Clone)]
     pub struct State{
         pub pos: i32,
         pub tree: Vec<Tree>
+    }
+
+    impl State{
+        pub fn set(&mut self, other: State){
+            *self = other;
+        }
     }
 }
